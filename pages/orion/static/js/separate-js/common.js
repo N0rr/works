c5b4b1446f39c2287ebe506947f54a45f8298afa
N0rr <xt12pabeh@mail.ru>
2016-12-24 20:45:56 +0300

    orion

---
+++ pages/orion/static/js/separate-js/common.js
@@ -0,0 +1,22 @@
+'use strict';
+
+$(document).ready(function () {  
+  $( function() {
+    $("#accordion-1").accordion({
+      collapsible: true,
+      icons: false      
+    });
+  });
+    $( function() {
+    $("#accordion").accordion({
+      collapsible: true,
+      icons: false      
+    });
+  });
+});
+
+
+
+
+
+
